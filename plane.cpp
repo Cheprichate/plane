2041fe38c9fc96f5c7d3721785e583d8a799b3c5
Cheprichate <petrovich15071998@mail.ru>
2023-10-03 21:58:43 +0400

    Добавьте файлы проекта.

---
+++ plane.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+int height;
+int speed;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Введите высоту полета : \n";
+    std::cin >> height;
+    std::cout << "Введите сокрость самолёта : \n";
+    std::cin >> speed;
+
+    if (( height >= 9000 && height <= 9501 ) && ( speed >= 750 && speed <= 850 )){
+        std::cout << "Курс верный. \n";
+    }
+    else {
+        std::cout << "Скорректируйте курс. \n";
+    }
+}
